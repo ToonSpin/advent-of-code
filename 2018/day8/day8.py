cd4b5aeeacdd7776e1d8a98e185400c6ba2c9465
Toon Spin <toon.spin@gmail.com>
2018-12-08 07:53:55 +0100

    Day 8

---
+++ 2018/day8/day8.py
@@ -0,0 +1,51 @@
+import fileinput
+
+input = []
+
+for line in fileinput.input():
+    input = line.rstrip().split(" ")
+input = [int(s) for s in input]
+
+
+class Day7TreeNode:
+    children = []
+    metadata = []
+    inputSize = 0
+
+    @staticmethod
+    def createTree(input):
+        numMetadata = input[1]
+        children = []
+        inputOffset = 2
+
+        for i in range(input[0]):
+            child, childInputOffset = Day7TreeNode.createTree(input[inputOffset:])
+            children.append(child)
+            inputOffset += childInputOffset
+
+        tree = Day7TreeNode(input[inputOffset:inputOffset + numMetadata])
+        tree.children = children
+
+        return (tree, inputOffset + numMetadata)
+
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+    def getValue(self):
+        if len(self.children) == 0:
+            return sum(self.metadata)
+
+        sumOfValues = 0
+        for reference in self.metadata:
+            if reference <= len(self.children):
+                sumOfValues += self.children[reference - 1].getValue()
+        return sumOfValues
+
+    def getDeepSumOfMetadata(self):
+        return sum(self.metadata) + sum([child.getDeepSumOfMetadata() for child in self.children])
+
+
+tree, inputSize = Day7TreeNode.createTree(input)
+
+print("Sum of all metadata:", tree.getDeepSumOfMetadata())
+print("Value:", tree.getValue())
